27a310c131a2f4562b13e98f0eac1d2f6786b366
dinayahia171 <dinayahia157@gmail.com>
2023-08-13 02:41:05 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - Entry point
+* Description: c program
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+for (char ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
