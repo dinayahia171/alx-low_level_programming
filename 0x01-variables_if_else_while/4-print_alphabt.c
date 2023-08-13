7774a82dd0726d53b019c8cfc196a88369e12a22
dinayahia171 <dinayahia157@gmail.com>
2023-08-13 03:49:27 -0700

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: c program
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+while (ch <= 'z')
+{
+	if (ch == 'e' || ch == 'q')
+		ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
+
