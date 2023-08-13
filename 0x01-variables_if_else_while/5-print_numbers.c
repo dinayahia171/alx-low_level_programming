d8b11631408d619f711d4b3911e60636a926f399
dinayahia171 <dinayahia157@gmail.com>
2023-08-13 04:05:33 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: c program
+* Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+while (digit <= 9)
+{
+	printf("%i", digit);
+digit++;
+}
+printf("\n");
+
+return (0);
+}
