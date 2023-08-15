9cc28a41af21d85c916edd07b6633110b4953851
dinayahia171 <dinayahia157@gmail.com>
2023-08-15 06:56:43 -0700

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,7 @@
 * Description: c program
 * Return: always 0 (Success)
 */
-int main(void)
-{
+
 void print_alphabet(void)
 {
 char ch;
@@ -14,5 +13,4 @@ for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
 }
-return (0);
-}
+
