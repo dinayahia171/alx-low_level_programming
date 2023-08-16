311602f5b47988ca9a7720e9ef7189b5c0426af5
dinayahia171 <dinayahia157@gmail.com>
2023-08-16 01:23:08 -0700

    task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *int_abs - function that computes the absolute value
+ * @n: takes integer type
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+if (n < 0)
+return (-n);
+else
+{
+return (n);
+}
+}
