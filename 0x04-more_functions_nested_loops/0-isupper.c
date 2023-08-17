9b7d395c366454992f11c9fbeace81e8190e4aa4
dinayahia171 <dinayahia157@gmail.com>
2023-08-17 03:23:23 -0700

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper-check if c is uppercase
+ * @c: input
+ * Return: 1 if c is uppercase, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
