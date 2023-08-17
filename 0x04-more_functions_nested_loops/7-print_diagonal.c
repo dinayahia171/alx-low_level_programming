1509103c53da1dd65d2889027784bec747b443a6
dinayahia171 <dinayahia157@gmail.com>
2023-08-17 13:15:41 -0700

    task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal- print line
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int i, x;
+
+if (n == 0)
+-purchar('\n');
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (x = 1; x <= n; x++)
+-purchar(' ');
+-purchar(92);
+-purchar('\n');
+}
+}
+}
