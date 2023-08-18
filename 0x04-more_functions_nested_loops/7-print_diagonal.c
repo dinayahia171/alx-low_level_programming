f1407ea3b7186beb9de542af0ad8221b4a9cd623
dinayahia171 <dinayahia157@gmail.com>
2023-08-18 05:40:16 -0700

    task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,15 @@ void print_diagonal(int n)
 int i, x;
 
 if (n <= 0)
-_purchar('\n');
+_putchar('\n');
 else
 {
 for (i = 1; i <= n; i++)
 {
 for (x = 1; x <= i; x++)
-_purchar(' ');
-_purchar(92);
-_purchar('\n');
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
 }
 }
 }
