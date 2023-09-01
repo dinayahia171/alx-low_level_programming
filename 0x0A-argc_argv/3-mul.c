a717d4999884f8b08ae41d2c08cd12e0daad8902
dinayahia171 <dinayahia157@gmail.com>
2023-09-01 15:43:01 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- print program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+int sum;
+
+if (argc == 3)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
