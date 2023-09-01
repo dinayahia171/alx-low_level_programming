44c6ef82131fe0a0e78e53f9854ffe009e8b5215
dinayahia171 <dinayahia157@gmail.com>
2023-09-01 16:10:25 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main- print program
  * @argc: int
  * @argv: list
  * Return: 0
  */
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 int sum = 0;
-char *c;
 
-while (--argc)
+for (int i = 1; i < argc; i++)
 {
-for (c = argv[argc]; *c; c++)
-if (*c < '0' || *c > '9')
-return (printf("Error\n"), 1);
-sum += atoi(argv[argc]);
-}
+for (int j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
 printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+printf("%d\n", sum);
+}
 return (0);
 }
