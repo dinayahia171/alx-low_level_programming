7ee10c26b8c0f8dca05335043150d8c419dbe00b
dinayahia171 <dinayahia157@gmail.com>
2023-09-05 14:21:06 -0700

    task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid- print program
+ * @width: int
+ * @height: list
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+int **tab, i, j;
+
+tab = malloc(sizeof(*tab) * height);
+
+if (width <= 0 || height <= 0 || tab == 0)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < height; i++)
+{
+tab[i] = malloc(sizeof(**tab) * width);
+if (tab[i] == 0)
+{
+while (white--)
+free(tab[i]);
+free(tab);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+tab [i][j] = 0;
+}
+}
+return (tab);
+}
