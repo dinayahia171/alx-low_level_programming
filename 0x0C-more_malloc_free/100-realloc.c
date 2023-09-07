e9c7bbc30dbcfb125470e3dbe9395024f333f6ef
dinayahia171 <dinayahia157@gmail.com>
2023-09-07 13:10:09 -0700

    task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_realloc- check the code
+ * @ptr: pointer
+ * @old_size: pointer
+ * @new_size: pointer
+ * Return: Always 0.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *p;
+unsigned int i;
+
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+return (p);
+}
+if (new_size > old_size)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < old_size && i < new_size; i++)
+*((char *)p + i) = *((char *)ptr + i);
+free(ptr);
+}
+return (p);
+}
