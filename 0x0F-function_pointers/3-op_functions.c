4b14d93c43716005b042341244558a34bb8fc916
dinayahia171 <dinayahia157@gmail.com>
2023-09-13 05:53:57 -0700

    task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add- add two int
+ * @a: frist int
+ * @b: second int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- add two int
+ * @a: frist int
+ * @b: second int
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- add two int
+ * @a: frist int
+ * @b: second int
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- add two int
+ * @a: frist int
+ * @b: second int
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- add two int
+ * @a: frist int
+ * @b: second int
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
