44ffb0b3a3284a8b6e37495c20ae40ab68550230
dinayahia171 <dinayahia157@gmail.com>
2023-09-15 07:30:23 -0700

    task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ if (d)
 {
 printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
-printf("Ower: %s\n", d->ower ? d->ower : "(nil)");
+printf("Ower: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
