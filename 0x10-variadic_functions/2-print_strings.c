d251cd41b7e5d0709f19a7f67209ddce57af5a99
dinayahia171 <dinayahia157@gmail.com>
2023-09-15 03:11:10 -0700

    task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,21 +7,22 @@
  * @n: number of arguments
  * @...: int
  * main: check code
- * Return: Always void
+ * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i = n;
-	va_list ap;
+int i = n;
+char *str;
+va_list ap;
 
-	if (!n)
-	{
-		printf("\n");
-		return;
-	}
-	va_start(ap, n);
-	while (i--)
-		printf("%d%s", va_arg(ap, int),
-			       i ? (separator ? separator : "") : "\n");
+if (!n)
+{
+printf("\n");
+return;
+}
+va_start(ap, n);
+while (i--)
+printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
+i ? (separator ? separator : "") : "\n");
 va_end(ap);
 }
