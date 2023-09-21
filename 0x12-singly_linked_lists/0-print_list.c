80c943418d179a4f497b31f0704e0de5b3eae849
dinayahia171 <dinayahia157@gmail.com>
2023-09-21 02:45:53 -0700

    task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * _strlen- raturns the length
+ * @s: the string
+ * Return: intger length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list- print print a linked list
+ * @h: pointer to the first node
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
