1066b62c5392f744a01ba057e6a2d57781001753
dinayahia171 <dinayahia157@gmail.com>
2023-09-21 02:53:39 -0700

    task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * list_len - determines the length
+ * @h: pointer to the first node
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
