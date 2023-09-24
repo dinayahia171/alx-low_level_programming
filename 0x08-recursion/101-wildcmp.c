53e5c97b8171bb403d21c9c9ca0a81672c9fcb12
dinayahia171 <dinayahia157@gmail.com>
2023-09-24 11:06:50 -0700

    task

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * move_past_start - iterates past
+ * @s2: the second
+ *
+ * Return: the pointer
+ */ 
+char *move_past_start(char *s2)
+{
+	if (*s2 == '*')
+return (move_past_strat(s2 + 1));
+else
+return (s2);
+}
+ /**
+ * inception - check code
+ * @s1: the frist string
+ * @s2: string
+ * Return: 1
+ */
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+if (*s1 == 0)
+return (0);
+if (*s1 == *s2)
+ret += wildcmp(s1 + 1, s2 + 1);
+ret += inception(s1 + 1, s2);
+return (ret);
+}
+/**
+ * wildcmp - check code
+ * @s1: string
+ * @s2: string
+ * Return: 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+int ret = 0;
+
+if (!*s1 && *s2 == '*' && !*move_past_start(s2))
+return (1);
+if (*s1 == *s2)
+{
+if (!*s1)
+return (1);
+return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+}
+if (!*s1 || !s2)
+return (0);
+if (*s2 == '*')
+{
+s2 = move_past_start(s2);
+if (!*s2)
+return (1);
+if (*s1 == *s2)
+ret += wildcmp(s1 + 1, s2 + 1);
+ret += inception9s1, s20;
+return (!!ret);
+}
+return (0);
+}
