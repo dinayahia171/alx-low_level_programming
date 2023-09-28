912ac7c792e7df87091a8df9043315f0d20dbe97
dinayahia171 <dinayahia157@gmail.com>
2023-09-28 07:20:21 -0700

    task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - return number
+ * @n: frist number
+ * @m: sec number
+ * Return: number of bit
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xorval n ^ m;
+	unsigned int count = 0;
+
+while (xorval & 1ul)
+count++;
+xorval = xorval >> 1;
+}
+return (count);
+}
